6369a34b8229302545251bb04df64d44692f9247
Paige Nelmes <designer@paigenelmes.com>
2023-01-03 13:48:25 -0800

    Add first set of seeds

---
+++ db/seeds/01_organizations_seeds.sql
@@ -0,0 +1,2 @@
+-- Organization table seeds here
+INSERT INTO organizations (name) VALUES ('Google');
---
+++ db/seeds/02_passwords_seeds.sql
@@ -0,0 +1,2 @@
+-- Passwords table seeds here
+INSERT INTO passwords (title, login, password, website, category) VALUES ('Instagram', 'hipaigenelmes', 'Strawberry456', 'https://www.instagram.com', 'Social');
