97fcd2cf7c5def7b405008baab275b2a31bcd79e
Sarah Campbell <sarahcampbell4646@gmail.com>
2023-01-04 16:03:30 -0500

    Add new on click function

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,17 +1,32 @@
 // Client facing scripts here
-$(() => {
-  $('#fetch-users').on('click', () => {
-    $.ajax({
-      method: 'GET',
-      url: '/api/users'
-    })
-    .done((response) => {
-      const $usersList = $('#users');
-      $usersList.empty();
 
-      for(const user of response.users) {
-        $(`<li class="user">`).text(user.name).appendTo($usersList);
-      }
-    });
+// $('#fetch-users').on('click', () => {
+//   $.ajax({
+//     method: 'GET',
+//     url: '/api/users'
+//   })
+//   .done((response) => {
+//     const $usersList = $('#users');
+//     $usersList.empty();
+
+//     for(const user of response.users) {
+//       $(`<li class="user">`).text(user.name).appendTo($usersList);
+//     }
+//   });
+// });
+
+
+
+// Client facing scripts here
+$(document).ready(function() {
+
+  $(".add-new").on("click", function() {
+    if($('.new-password').css('display') === 'none') {
+      $('.new-password').css("display", "flex");
+      $('.new-password').slideDown('slow');
+    } else {
+      $('.new-password').css("display", "none");
+    }
   });
 });
+
