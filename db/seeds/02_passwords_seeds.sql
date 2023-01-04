bc92717e48398b1f21181e07a0d5fb842f0461bc
Paige Nelmes <designer@paigenelmes.com>
2023-01-04 08:59:10 -0800

    Add more seeds

--- db/seeds/02_passwords_seeds.sql
+++ db/seeds/02_passwords_seeds.sql
@@ -1,2 +1,7 @@
 -- Passwords table seeds here
-INSERT INTO passwords (title, login, password, website, category) VALUES ('Instagram', 'hipaigenelmes', 'Strawberry456', 'https://www.instagram.com', 'Social');
+INSERT INTO passwords (title, login, password, website, category, organization_id) VALUES ('Instagram', 'hipaigenelmes', 'WarmBlanket74#', 'https://www.instagram.com', 'Social', 1);
+INSERT INTO passwords (title, login, password, website, category, organization_id) VALUES ('Reddit', 'littlemaihall', 'CatNip23$', 'https://www.reddit.com', 'Social', 2);
+INSERT INTO passwords (title, login, password, website, category, organization_id) VALUES ('Netflix', 'itsdavidhall', 'SweetChili2008*', 'https://www.netflix.com', 'Entertainment', 2);
+INSERT INTO passwords (title, login, password, website, category, organization_id) VALUES ('Crave', 'bigboybarley', 'LongWalks@247', 'https://www.crave.ca', 'Entertainment', 1);
+INSERT INTO passwords (title, login, password, website, category, organization_id) VALUES ('Amazon', 'pollywalnuts', 'Fur$$Coat10', 'https://www.amazon.ca', 'Shopping', 1);
+INSERT INTO passwords (title, login, password, website, category, organization_id) VALUES ('Uniqlo', 'fashionablekeiko', 'Lets#Chill345', 'https://www.uniqlo.com/ca/en/', 'Shopping', 2);
