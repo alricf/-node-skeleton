543a99c6b744777f8abeb08b011138add3736986
Sarah Campbell <sarahcampbell4646@gmail.com>
2023-01-04 14:34:58 -0500

    Fix styling, slidedown new pass div

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
 // Client facing scripts here
+$(document).ready(function() {
+
+  $(".add-new").on("click", function() {
+
+    if($('.new-password').css('display') === 'none') {
+      $('.new-password').css("display", "flex");
+      $('.new-password').slideDown('slow');
+    } else {
+      $('.new-password').css("display", "none");
+    }
+  });
+});
