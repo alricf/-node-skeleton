4d4213a4fc7c1f50b1c816c177aac78effce2311
tronross <john_ross@analogforever.ca>
2023-01-05 17:22:56 -0500

    Add /api/passwords/ route

---
+++ routes/passwords-api.js
@@ -0,0 +1,27 @@
+/*
+ * All routes for Passwords Data are defined here
+ * Since this file is loaded in server.js into api/organizations,
+ *   these routes are mounted onto /api/widgets
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+
+const express = require('express');
+const router  = express.Router();
+// const db = require('../db/connection');
+const passwordQueries = require('../db/queries/passwords_by_org');
+
+router.get('/api/passwords/', (req, res) => {
+  passwordQueries.getPasswordsByOrg()
+  .then(data => {
+    const passwords = data.rows;
+    res.json({ passwords });
+  })
+  .catch(err => {
+    res
+      .status(500)
+      .json({ error: err.message });
+  });
+});
+
+module.exports = router;
