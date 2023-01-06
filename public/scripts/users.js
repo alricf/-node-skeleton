e8aa686fe6163d7bf9a14ad95905e457bbf6a6ee
Sarah Campbell <sarahcampbell4646@gmail.com>
2023-01-06 15:00:43 -0500

    Add slider for password generator length

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -18,26 +18,52 @@ $(document).ready(function() {
   });
 
 
-  //Add new button click, shows inputs for creating new password info
+  //Add new - button click, displays inputs for creating new password info
   $(".add-new").on("click", function() {
+    let titleInput = document.getElementById('new-title');
+    let userInput = document.getElementById('new-user');
+    let websiteInput = document.getElementById('new-website');
+    let categoryInput = document.getElementById('new-category');
+    let passInput = document.getElementById('new-password');
+
     if($('.new-password-box').css('display') === 'none') {
       $('.new-password-box').css("display", "flex");
       $('.new-password-box').slideDown('slow');
     } else {
       $('.new-password-box').css("display", "none");
+      titleInput.value = '';
+      userInput.value = '';
+      websiteInput.value = '';
+      categoryInput.value = '';
+      passInput.value = '';
     }
   });
 
-  $(".edit-button").on("click", function() {
-    $('.new-pass-input').css("visibility", "visible");
-    this.textContent = "Save";
-  });
+  //Edit button function
+  // $(".edit-button").on("click", function() {
+  //   $('.new-pass-input').css("visibility", "visible");
+  //   this.textContent = "Save";
+  // });
+
+  //Change range slider label based on user selection
+  const slider = document.getElementById('range');
+
+  slider.onchange = function() {
+    let sliderVal = slider.value;
+    let rangeNum = document.getElementById('rangeNum');
+
+    rangeNum.textContent = sliderVal;
+
+  };
+
+
+
 
   //Generate new password based on requirements, place in password input
   $(".generate-button").on("click", function() {
-
     let passInput = document.getElementById('new-password');
-
+    let slider = document.getElementById('range');
+    // console.log(slider.value);
     //get array of checked box values
     const checkboxes = document.querySelectorAll('input[name="pass-req"]:checked');
     let checks = [];
@@ -51,8 +77,7 @@ $(document).ready(function() {
     const lowercase = charCodes.map(code => String.fromCharCode(code));
     const uppercase = lowercase.map(letter => letter.toUpperCase());
 
-
-
+    //generate password function
     const generatePassword = (checks) => {
       const availableCharacters = [];
 
@@ -60,21 +85,25 @@ $(document).ready(function() {
       checks.includes("Numbers") ? availableCharacters.push(numbers) : [];
       checks.includes("Uppercase") ? availableCharacters.push(uppercase) : [];
       checks.includes("Lowercase") ? availableCharacters.push(lowercase) : [];
+
       //generated pasword
       let pass = [];
 
-      for (let i = 0; i < 12; i + availableCharacters.length) {
-        for (let j = 0; j < availableCharacters.length; j++) {
-          pass.push(availableCharacters[j][Math.floor(Math.random() * availableCharacters[j].length)]);
+      if (availableCharacters.length >= 1) {
+        while (pass.length < slider.value) {
+
+          for (let i = 0; i < availableCharacters.length; i++) {
+            pass.push(availableCharacters[i][Math.floor(Math.random() * availableCharacters[i].length)]);
+
+            if (pass.length == slider.value) {
+              const scramblePass = pass.sort(() => Math.random() - 0.5).join('');
+              return (passInput.value = scramblePass);
+            }
+          }
         }
+      } else {
+        return (passInput.value = "");
       }
-      console.log(pass);
-      // const scramblePass = pass.sort(() => Math.random() - 0.5).join('');
-      // passInput.value = scramblePass;
-      // console.log(scramblePass);
-      return;
-
-
     };
 
     generatePassword(checks);
