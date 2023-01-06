9cb3c1bb7be34af4371340c2213551799daf910d
Paige Nelmes <designer@paigenelmes.com>
2023-01-06 12:36:16 -0800

    Add new queries for password categories

---
+++ db/queries/passwords_by_entertainment.js
@@ -0,0 +1,13 @@
+const db = require('../connection');
+
+const getPasswordsByEntertainment = () => {
+  return db.query(`SELECT title, login, password FROM passwords WHERE organization_id = 1 AND category = 'Entertainment';`)
+    .then(data => {
+      return data.rows;
+    })
+    .catch((error => {
+      console.log(error);
+    }));
+};
+
+module.exports = { getPasswordsByEntertainment };
--- db/queries/passwords_by_org.js
+++ db/queries/passwords_by_org.js
@@ -3,7 +3,6 @@ const db = require('../connection');
 const getPasswordsByOrg = () => {
   return db.query('SELECT title, login, password FROM passwords WHERE organization_id = 1;')
     .then(data => {
-      console.log(data.rows);
       return data.rows;
     })
     .catch((error => {
