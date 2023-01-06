5ffc124d85edaece51aa072d1820e22f51bc8c99
Paige Nelmes <designer@paigenelmes.com>
2023-01-06 13:46:38 -0800

    Edit Create New Password query

--- db/queries/create_new_password.js
+++ db/queries/create_new_password.js
@@ -2,7 +2,7 @@ const db = require('../connection');
 
 const createNewPassword = (password) => {
   return db.query(`INSERT INTO passwords(title, login, password, website, category, organization_id)
-    VALUES ($1, $2, $3, $4, $5, $6)
+    VALUES ($1, $2, $3, $4, $5, 1)
     RETURNING *;
     `,
   [password.title, password.login, password.password, password.website, password.category, password.organization_id])
