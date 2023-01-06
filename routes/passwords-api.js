9f414f1d69989af2ade52af47e4fb04409a4d92a
tronross <john_ross@analogforever.ca>
2023-01-06 15:43:17 -0500

    Add ROUTE api/passwords/work

--- routes/passwords-api.js
+++ routes/passwords-api.js
@@ -9,11 +9,16 @@
 const express = require('express');
 const router  = express.Router();
 // const passwordQueries = require('../routes/queryIndices/apiPasswordsQueriesIndex');
-const ogPasswordQueries = require('../db/queries/passwords_by_org');
+
+const byOrg = require('../db/queries/passwords_by_org');
+const byWork = require('../db/queries/passwords_by_work');
+// const byFinances = require('../db/queries/passwords_by_finances');
+// const bySocialMedia = require('../db/queries/passwords_by_social-media');
+// const byEntertainment = require('../db/queries/passwords_by_entertainment');
 
 // GET passwords/api to retrieve title, login and password for all logins for one organization
 router.get('/', (req, res) => {
-  ogPasswordQueries.getPasswordsByOrg()
+  byOrg.getPasswordsByOrg()
   .then(passwords => {
     res.json({ passwords });
   })
@@ -26,7 +31,7 @@ router.get('/', (req, res) => {
 
 // GET passwords/api/work to retrieve title, login and password for all logins categorized as 'work' for one organization
 router.get('/work', (req, res) => {
-  ogPasswordQueries.getPasswordsByOrg()
+  byWork.getPasswordsByWork()
   .then(passwords => {
     res.json({ passwords });
   })
@@ -37,18 +42,45 @@ router.get('/work', (req, res) => {
   });
 });
 
-// GET passwords/api/finances to retrieve title, login and password for all logins categorized as 'finances' for one organization
-router.get('/finances', (req, res) => {
-  ogPasswordQueries.getPasswordsByOrg()
-  .then(passwords => {
-    res.json({ passwords });
-  })
-  .catch(err => {
-    res
-      .status(500)
-      .json({ error: err.message });
-  });
-});
+// // GET passwords/api/finances to retrieve title, login and password for all logins categorized as 'finances' for one organization
+// router.get('/finances', (req, res) => {
+//   byFinances.getPasswordsByFinances()
+//   .then(passwords => {
+//     res.json({ passwords });
+//   })
+//   .catch(err => {
+//     res
+//       .status(500)
+//       .json({ error: err.message });
+//   });
+// });
+
+// // GET passwords/api/social-media to retrieve title, login and password for all logins categorized as 'social-media' for one organization
+// router.get('/social-media', (req, res) => {
+//   ogPasswordQueries.getPasswordsByOrg()
+//   .then(passwords => {
+//     res.json({ passwords });
+//   })
+//   .catch(err => {
+//     res
+//       .status(500)
+//       .json({ error: err.message });
+//   });
+// });
+
+// // GET passwords/api/entertainment to retrieve title, login and password for all logins categorized as 'entertainment' for one organization
+// router.get('/entertainment', (req, res) => {
+//   ogPasswordQueries.getPasswordsByOrg()
+//   .then(passwords => {
+//     res.json({ passwords });
+//   })
+//   .catch(err => {
+//     res
+//       .status(500)
+//       .json({ error: err.message });
+//   });
+// });
+
 
 
 module.exports = router;
