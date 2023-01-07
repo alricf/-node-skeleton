48fdfd0e4308cfc722c99fc7fb14a5176f4ee1d9
tronross <john_ross@analogforever.ca>
2023-01-07 14:18:05 -0500

    Fix createNewPassword query

--- db/queries/create_new_password.js
+++ db/queries/create_new_password.js
@@ -1,11 +1,10 @@
 const db = require('../connection');
 
 const createNewPassword = (password) => {
+  console.log(password);
   return db.query(`INSERT INTO passwords(title, login, password, website, category, organization_id)
     VALUES ($1, $2, $3, $4, $5, 1)
-    RETURNING *;
-    `,
-  [password.title, password.login, password.password, password.website, password.category, password.organization_id])
+    RETURNING *;`, [password.title, password.login, password.password, password.website, password.category])
     .then(data => {
       return data.rows;
     })
