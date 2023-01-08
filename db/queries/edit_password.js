8960c0a23ac95ec0ae1c349b785ac0b74d60bd17
Paige Nelmes <designer@paigenelmes.com>
2023-01-08 12:50:56 -0800

    Fix update passwords query

--- db/queries/edit_password.js
+++ db/queries/edit_password.js
@@ -4,7 +4,7 @@ const editPassword = (password) => {
   console.log(password);
   return db.query(`UPDATE passwords
   SET title = $1, login = $2, password = $3, website = $4, category = $5
-  WHERE organization_id = 1)
+  WHERE organization_id = 1 AND id = 1
   RETURNING *;`, [password.title, password.login, password.password, password.website, password.category])
     .then(data => {
       return data.rows;
