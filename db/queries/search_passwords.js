23f913b6bce2f8dfed12f149c1bfc213c8caf650
Paige Nelmes <designer@paigenelmes.com>
2023-01-09 12:16:32 -0800

    Fix search passwords query

--- db/queries/search_passwords.js
+++ db/queries/search_passwords.js
@@ -1,9 +1,9 @@
 const db = require('../connection');
 
-const searchPasswords = () => {
+const searchPasswords = (searchTerm) => {
   return db.query(`SELECT title, login, website, password FROM passwords
   WHERE organization_id = 1 AND title LIKE $1 OR login LIKE $1 OR website LIKE $1 OR password LIKE $1
-  ORDER BY id DESC;`)
+  ORDER BY id DESC;`, [searchTerm])
     .then(data => {
       return data.rows;
     })
