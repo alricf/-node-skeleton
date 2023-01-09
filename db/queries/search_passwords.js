cdd61c6be31bedfba61dbd348601fe94df86273d
ACF <alricfernandes11@gmail.com>
2023-01-09 16:19:04 -0500

    Updating function name

--- db/queries/search_passwords.js
+++ db/queries/search_passwords.js
@@ -1,6 +1,6 @@
 const db = require('../connection');
 
-const searchPasswords = (searchTerm) => {
+const getPasswordSearch = (searchTerm) => {
   return db.query(`SELECT title, login, website, password FROM passwords
   WHERE organization_id = 1 AND title LIKE $1 OR login LIKE $1 OR website LIKE $1 OR password LIKE $1
   ORDER BY id DESC;`, [searchTerm])
@@ -12,4 +12,4 @@ const searchPasswords = (searchTerm) => {
     }));
 };
 
-module.exports = { searchPasswords };
+module.exports = { getPasswordSearch };
