d4127a645897f76aef4be92a665e9674f3fe5b9e
tronross <john_ross@analogforever.ca>
2023-01-09 19:23:33 -0500

    Fix search_passwords to use HAVING organization_id

--- db/queries/search_passwords.js
+++ db/queries/search_passwords.js
@@ -2,7 +2,9 @@ const db = require('../connection');
 
 const getPasswordSearch = (searchTerm) => {
   return db.query(`SELECT id, title, login, website, password, category FROM passwords
-  WHERE organization_id = 1 AND title LIKE $1 OR login LIKE $1 OR website LIKE $1 OR password LIKE $1
+  WHERE title LIKE $1 OR login LIKE $1 OR website LIKE $1 OR password LIKE $1
+  GROUP BY passwords.id
+  HAVING organization_id = 1
   ORDER BY id DESC;`, [`%${searchTerm}%`])
     .then(data => {
       return data.rows;
