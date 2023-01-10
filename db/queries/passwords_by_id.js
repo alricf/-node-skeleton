93953517d5e07637a40af0340597bc83555db7a9
tronross <john_ross@analogforever.ca>
2023-01-10 14:44:03 -0500

    Remove RETURNING * from query

--- db/queries/passwords_by_id.js
+++ db/queries/passwords_by_id.js
@@ -2,8 +2,7 @@ const db = require('../connection');
 
 const getPasswordById = (password) => {
   return db.query(`SELECT website FROM passwords
-  WHERE id = $1
-  RETURNING *;`, [password.id])
+  WHERE id = $1;`, [password.id])
     .then(data => {
       console.log(data.rows);
       return data.rows;
