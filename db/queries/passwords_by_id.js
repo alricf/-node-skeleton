b0d176a6912fe9f95633fa05e50291040fd64b95
tronross <john_ross@analogforever.ca>
2023-01-10 15:04:20 -0500

    Add specificity to return value (data.rows[0];)

--- db/queries/passwords_by_id.js
+++ db/queries/passwords_by_id.js
@@ -4,8 +4,8 @@ const getPasswordById = (password) => {
   return db.query(`SELECT website FROM passwords
   WHERE id = $1;`, [password])
     .then(data => {
-      console.log(data.rows);
-      return data.rows;
+      console.log(data.rows[0]);
+      return data.rows[0];
     })
     .catch((error => {
       console.log(error);
